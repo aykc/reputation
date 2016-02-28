3eef53e72a040cf2255f503423477f57aa87dd9c
Anton Berkutov <void__fastcall@mail.ru>
2016-02-28 23:56:29 +0300

    last changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :review
-  has_reputation :points, source: :user
+  has_reputation :points, source: :user, source_of: [ reputation: :comment_points, of: :user ]
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
+  has_many :comments
   has_reputation :points, source: :user, source_of: [ reputation: :review_points, of: :user ]
 end
